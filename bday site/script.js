cf6351ae62b2df8957116e52246b02ae3ebb1af5
apts7 <aptsingh7@gmail.com>
2025-06-27 23:58:51 +0530

    Add files via upload

---
+++ bday site/script.js
@@ -0,0 +1,31 @@
+// Confetti blast using Canvas Confetti
+window.onload = () => {
+  confetti({
+    particleCount: 500,
+    spread: 200,
+    origin: { y: 0.6 },
+  });
+
+  setTimeout(() => {
+    confetti({
+      particleCount: 250,
+      spread: 200,
+      origin: { y: 0.4 },
+    });
+  }, 1000);
+};
+window.onload = () => {
+  confetti({
+    particleCount: 500,
+    spread: 200,
+    origin: { y: 0.6 },
+  });
+
+  setTimeout(() => {
+    confetti({
+      particleCount: 250,
+      spread: 200,
+      origin: { y: 0.4 },
+    });
+  }, 1000);
+};
